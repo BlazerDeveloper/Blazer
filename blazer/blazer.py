dc73a4f420969a1dfdfcf499f37f8a029e6dc39c
unknown <trey.whitehead2000@gmail.com>
2014-03-18 18:51:11 -0700

    Created a function for loading images

--- blazer/blazer.py
+++ blazer/blazer.py
@@ -22,19 +22,21 @@ FACING_UPRIGHT = 6
 FACING_DWNRIGHT = 7
 
 pygame.init()
- 
-Icon = pygame.image.load("Icon.png")
+def load_image(filename):
+    pygame.image.load(os.path.join ("resources","filename"))
+
+Icon = load_image("Icon.png")
 pygame.display.set_caption("Blazer: Trails Unknown")
 pygame.display.set_icon(Icon)
 
 facings = [ pygame.image.load(os.path.join("resources","tux.png")), pygame.image.load(os.path.join("resources","tuxright.png")), pygame.image.load(os.path.join("resources","tuxdown.png")), pygame.image.load(os.path.join("resources","tuxleft.png")), pygame.image.load(os.path.join("resources","tuxupleft.png")), pygame.image.load(os.path.join("resources","tuxdwnlft.png")), pygame.image.load(os.path.join("resources","tuxupright.png")), pygame.image.load(os.path.join("resources","tuxdownright.png"))]
 
 screen = pygame.display.set_mode(DISPLAY_SIZE) 
-background1= pygame.image.load(os.path.join("resources",'background1.png'))
+background1= load_image('background1.png')
 background1_rect = background1.get_rect(centerx=400, centery=300)
 half_screen = vector.dividing(DISPLAY_SIZE, DIVISION)
 
-walltop = pygame.image.load(os.path.join("resources","walltop.png"))
+walltop = load_image("walltop.png")
 walltop_size = pygame.Surface.get_size(walltop)
 walltop_rect = walltop.get_rect(walltop_size)
 
@@ -43,16 +45,16 @@ tux_rect = tux.get_rect(TUX_STARTING_LOC)
 
 <<<<<<< HEAD
 
-button = pygame.image.load(os.path.join("resources",'Startbutton.png'))
+button = load_image('Startbutton.png')
 buttonRect = button.get_rect(centerx=400, centery=400)
 
-title = pygame.image.load(os.path.join("resources","title.png"))
+title = load_image("title.png")
 titleRect = title.get_rect(centerx=400, centery=200)
 
-button2 = pygame.image.load(os.path.join("resources",'Quitbutton.png'))
+button2 = load_image('Quitbutton.png')
 button2Rect = button.get_rect(centerx=400, centery=500)
 
-buttonp = pygame.image.load(os.path.join("resources","startbuttonp.png"))
+buttonp = load_image("startbuttonp.png")
 
 pygame.display.flip()
 
